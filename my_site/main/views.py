60c69a82042b839e4571ef540bb28d7db68cb2c2
annc4st <annaseniuta@gmail.com>
2023-12-11 20:21:37 +0000

    user

--- my_site/main/views.py
+++ my_site/main/views.py
@@ -7,40 +7,50 @@ from .forms import CreateNewList
 def index(response, id):
     ls  = ToDoList.objects.get(id=id)
     
-    if response.method =="POST":
-        print(response.POST)
-        #if your request os POST => get "save" button => all info is passed in dict
-        if response.POST.get("save"):
-            for item in ls.item_set.all():
-                if response.POST.get("c" + str(item.id)) == "clicked":
-                    item.complete = True # to update
+    if ls in response.user.todolist.all():
+        
+        if response.method =="POST":
+            print(response.POST)
+            #if your request os POST => get "save" button => all info is passed in dict
+            if response.POST.get("save"):
+                for item in ls.item_set.all():
+                    if response.POST.get("c" + str(item.id)) == "clicked":
+                        item.complete = True # to update
+                    else:
+                        item.complete = False
+                    item.save()
+                    
+            
+            elif response.POST.get("newItem"):
+                txt = response.POST.get("new-item-input")            
+                #validate
+                if len(txt) > 2:
+                    ls.item_set.create(text =txt, complete=False)
                 else:
-                    item.complete = False
-                item.save()
-                
-        
-        elif response.POST.get("newItem"):
-            txt = response.POST.get("new-item-input")            
-            #validate
-            if len(txt) > 2:
-                ls.item_set.create(text =txt, complete=False)
-            else:
-                print("Invalid input")
-        
-    return render(response, "main/list.html", {"ls": ls})
+                    print("Invalid input")
+            
+        return render(response, "main/list.html", {"ls": ls})
+    return render(response, "main/view.html", {})
 
 def home(response):
      return render(response, "main/home.html", {})
 
 def create(response):
+ 
     if response.method =="POST":
         form = CreateNewList(response.POST)  #create dict
+        
         if form.is_valid():
             name_cleaned_data = form.cleaned_data["name"]
-            t = ToDoList(name = name_cleaned_data)
+            t=ToDoList(name=name_cleaned_data)  
+           
             t.save()
+            response.user.todolist.add(t)
         
         return HttpResponseRedirect("/%i" %t.id) # redirection to the page to display persons list
     else:
         form = CreateNewList()
     return render(response, "main/create.html", {"form": form})
+
+def view(response):
+    return render(response, "main/view.html", {})
